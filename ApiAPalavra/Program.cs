cebeae8f6ebcf02d469f414ac5e7b54f367c68b2
Ranyere <Ranyere@DESKTOP-5EIPQ1S>
2025-06-06 18:51:29 -0300

    Correções na geração da documentação

--- ApiAPalavra/Program.cs
+++ ApiAPalavra/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using ApiAPalavra.Services.LivroBiblicoService;
 using ApiAPalavra.Services.VersiculoBiblicoService;
 
@@ -8,7 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddSingleton<ILivroBiblicoService, LivroBiblicoService>();
 builder.Services.AddSingleton<IVersiculoBiblicoService, VersiculoBiblicoService>();
